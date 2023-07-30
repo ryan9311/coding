33897d562bff8bde3a04013527437b7a60b1f86b
York_Ki <p.ryan9311@gmail.com>
2023-07-30 22:34:44 +0900

    컴포넌트 만들기

--- react/shop/src/App.js
+++ react/shop/src/App.js
@@ -1,8 +1,30 @@
-import "./App.css";
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import "./App.css";
+import data from "./data";
+import ShoesCard from "./shoesCard";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Ryan_Shop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className="mainBg"></div>
+      <div className="container">
+        <div className="row">
+          <ShoesCard></ShoesCard>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
