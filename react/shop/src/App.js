0865349f6fa26c3988e9c32fd11eca89dccdac68
York_Ki <p.ryan9311@gmail.com>
2023-07-30 23:06:55 +0900

    props, map 사용해서 컴포넌트 반복 데이터 바인딩

--- react/shop/src/App.js
+++ react/shop/src/App.js
@@ -1,11 +1,12 @@
-import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Nav, Navbar } from "react-bootstrap";
-import "./App.css";
+import { useState } from "react";
 import data from "./data";
-import ShoesCard from "./shoesCard";
+
+import "./App.css";
 
 function App() {
+  let [shoes] = useState(data);
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark">
@@ -20,11 +21,32 @@ function App() {
       <div className="mainBg"></div>
       <div className="container">
         <div className="row">
-          <ShoesCard></ShoesCard>
+          {shoes.map((a, i) => {
+            return <Card shoes={shoes[i]} i={i}></Card>;
+          })}
         </div>
       </div>
     </div>
   );
+
+  function Card(props) {
+    return (
+      <>
+        <div className="col-md-4">
+          <img
+            src={
+              "https://codingapple1.github.io/shop/shoes" +
+              (props.i + 1) +
+              ".jpg"
+            }
+            width="80%"
+          />
+          <h4>{props.shoes.title}</h4>
+          <p>{props.shoes.price}</p>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
