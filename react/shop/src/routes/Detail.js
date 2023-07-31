f7c190498a7cf9034c6d812e6036994717ed6b4c
York_Ki <p.ryan9311@gmail.com>
2023-07-31 22:17:16 +0900

    useEffect 를 활용한 2초 뒤 사라지는 div 만들기

--- react/shop/src/routes/Detail.js
+++ react/shop/src/routes/Detail.js
@@ -1,4 +1,10 @@
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+// 동적이 UI 만드는 방법
+//1. UI 상태를 저장할 state 만들고
+//2. state에 따라서 UI가 어떻게 보일지 작성
 
 function Detail(props) {
   let { id } = useParams();
@@ -7,8 +13,27 @@ function Detail(props) {
     return x.id == id;
   });
 
+  let [alert, setAlert] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+  });
+
+  let [count, setCount] = useState(0);
   return (
     <div className="container">
+      {alert == true ? (
+        <div className="alert alert-warning">2초이내 구매시 할인!!</div>
+      ) : null}
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        버튼
+      </button>
       <div className="row">
         <div className="col-md-6">
           <img
