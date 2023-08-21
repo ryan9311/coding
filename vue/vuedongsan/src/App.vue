d83af1d86b65077552f6bbc4e72f63e79813440c
York_Ki <p.ryan9311@gmail.com>
2023-08-21 22:19:14 +0900

    v-for / @click 사용해보기

--- vue/vuedongsan/src/App.vue
+++ vue/vuedongsan/src/App.vue
@@ -1,17 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="menu">
+      <a v-for="(menuItem, i) in menus" :key="i">{{ menuItem }}</a>
+    </div>
+    <h2>원룸#</h2>
+
+    <div v-for="(productArea, i) in products" :key="i">
+      <h4>{{ productArea }}</h4>
+      <p>50 만원</p>
+      <button @click="reportCnt[i]++">허위매물신고</button>
+      <span> 신고 수 : {{ reportCnt[i] }}</span>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      reportCnt: [0, 0, 0],
+      menus: ["Home", "Products", "About"],
+      products: ["역삼동 원룸", "천호동 원룸", "마포구 원룸"],
+    };
+  },
+  methods: {
+    reportBtn(i) {
+      this.reportCnt[i]++;
+    },
+  },
+};
 </script>
 
 <style>
@@ -21,6 +39,20 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu > a {
+  color: white;
+  padding: 10px;
+}
+
+.menu > a:hover {
+  cursor: pointer;
 }
 </style>
