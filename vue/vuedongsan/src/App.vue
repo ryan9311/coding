067227be73199e234dcaebeacc98153b3de568f3
York_Ki <p.ryan9311@gmail.com>
2023-08-27 12:34:16 +0900

    이미지 추카

--- vue/vuedongsan/src/App.vue
+++ vue/vuedongsan/src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
+    <div class="black-bg" v-if="modalOpen == true">
+      <div class="white-bg">
+        <h4>상세페이지</h4>
+        <p>상세페이지내용임</p>
+        <button @click="modalOpen = false">닫기</button>
+      </div>
+    </div>
+
     <div class="menu">
       <a v-for="(menuItem, i) in menus" :key="i">{{ menuItem }}</a>
     </div>
     <h2>원룸#</h2>
 
     <div v-for="(productArea, i) in products" :key="i">
-      <h4>{{ productArea }}</h4>
+      <img src="./assets/room0.jpg" />
+      <h4 class="productTitle" @click="modalOpen = true">{{ productArea }}</h4>
       <p>50 만원</p>
       <button @click="reportCnt[i]++">허위매물신고</button>
       <span> 신고 수 : {{ reportCnt[i] }}</span>
@@ -19,6 +28,7 @@ export default {
   name: "App",
   data() {
     return {
+      modalOpen: false,
       reportCnt: [0, 0, 0],
       menus: ["Home", "Products", "About"],
       products: ["역삼동 원룸", "천호동 원룸", "마포구 원룸"],
@@ -41,6 +51,26 @@ export default {
   color: #2c3e50;
 }
 
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
 .menu {
   background: darkslateblue;
   padding: 15px;
@@ -53,6 +83,10 @@ export default {
 }
 
 .menu > a:hover {
+  cursor: pointer;
+}
+
+.productTitle:hover {
   cursor: pointer;
 }
 </style>
