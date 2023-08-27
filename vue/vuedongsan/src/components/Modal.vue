bf8f338a5c06cb91bde3651f32e60deb59177464
York_Ki <p.ryan9311@gmail.com>
2023-08-27 15:40:53 +0900

    모달 안에 개월 수 입력 및 watch를 사용하여 input 안에 값 제어하기

--- vue/vuedongsan/src/components/Modal.vue
+++ vue/vuedongsan/src/components/Modal.vue
@@ -5,6 +5,7 @@
       <h4>{{ onerooms[clickId].title }}</h4>
       <p>{{ onerooms[clickId].content }}</p>
       <input v-model="month" placeholder="개월 수 입력" />
+      <input v-model="month" type="range" min="1" max="12" />
       <p>{{ month }}개월 금액 : {{ onerooms[clickId].price * month }} 원</p>
       <button @click="$emit('modalClose')">닫기</button>
     </div>
@@ -18,6 +19,17 @@ export default {
     return {
       month: 1,
     };
+  },
+  watch: {
+    month(a) {
+      if (isNaN(a)) {
+        alert("숫자만 입력!");
+        this.month = 1;
+      } else if (a > 12) {
+        alert("최대 12개월 입니다.");
+        this.month = 12;
+      }
+    },
   },
   props: {
     onerooms: Array,
