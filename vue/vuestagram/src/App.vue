e8bba513381643c810f7414dbed8177c960517ca
York_Ki <p.ryan9311@gmail.com>
2023-09-02 08:43:23 +0900

    뷰스타그램 레이아웃 및 이미지 바인딩

--- vue/vuestagram/src/App.vue
+++ vue/vuestagram/src/App.vue
@@ -1,5 +1,22 @@
 <template>
+  <div class="header">
+    <ul>
+      <li class="cancelBtn">cancel</li>
+    </ul>
+    <img class="logo" src="./assets/vuestalog.png" alt="logo" />
+    <ul>
+      <li>Next</li>
+    </ul>
+  </div>
+
   <VuestaPost :vueSta="vueSta" />
+
+  <div class="footer">
+    <ul class="footer-button-plus">
+      <input type="file" id="file" class="inputfile" />
+      <label for="file" class="input-plus">+</label>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -26,6 +43,69 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
+}
+body {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+li {
+  list-style: none;
+  text-align: center;
+  font-weight: bold;
+  color: #9d44c0;
+  font-size: 20px;
+  cursor: pointer;
+}
+
+ul {
+  margin: 0;
+  padding: 10px;
+}
+
+.header {
+  position: sticky;
+  top: 0;
+  width: 600px;
+  height: 100px;
+  margin: 0 auto;
+  display: flex;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.logo {
+  width: 100px;
+}
+
+.footer {
+  width: 100%;
+  position: sticky;
+  bottom: 0;
+  padding-bottom: 10px;
+  background-color: white;
+}
+.footer-button-plus {
+  width: 80px;
+  margin: auto;
+  text-align: center;
+  cursor: pointer;
+  font-size: 24px;
+  padding-top: 12px;
+}
+.sample-box {
+  width: 100%;
+  height: 600px;
+  background-color: bisque;
+}
+.inputfile {
+  display: none;
+}
+.input-plus {
+  cursor: pointer;
 }
 </style>
--- vue/vuestagram/src/components/VuestaPost.vue
+++ vue/vuestagram/src/components/VuestaPost.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="postCard" v-for="(a, i) in vueSta" :key="i">
     <div class="profile">
-      <div
-        class="profileImg"
-        style="background-image: url(https://picsum.photos/100?random=3)"
-      ></div>
-      <div>{{ a.name }}</div>
-      <div>{{ a.date }}</div>
+      <div class="profileWrap">
+        <div
+          class="profileImg"
+          :style="{ backgroundImage: `url(${a.userImage})` }"
+        ></div>
+        <div class="name">{{ a.name }}</div>
+      </div>
+      <div class="date">{{ a.date }}</div>
     </div>
     <div
       class="postImg"
-      style="background-image: url(https://picsum.photos/600?random=5)"
+      :style="{ backgroundImage: `url(${a.postImage})` }"
     ></div>
+    <div class="likes">{{ a.likes }} LIKE</div>
     <div class="content">{{ a.content }}</div>
-    {{ console.log(a.userImage) }}
   </div>
 </template>
 
@@ -31,8 +33,11 @@ export default {
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
+  width: 600px;
+  margin: 0 auto;
   margin-bottom: 30px;
+  box-shadow: rgba(0, 0, 0, 0.05) 0px 2px 2px 0px;
 }
 
 .profile {
@@ -42,6 +47,13 @@ export default {
   gap: 20px;
   justify-content: center;
   margin-bottom: 10px;
+  justify-content: space-between;
+}
+
+.profileWrap {
+  display: flex;
+  gap: 20px;
+  align-items: center;
 }
 
 .profileImg {
@@ -57,7 +69,13 @@ export default {
   height: 600px;
 }
 
+.likes {
+  display: flex;
+  padding: 10px 5px;
+}
+
 .content {
   font-size: 20px;
+  padding: 10px 5px;
 }
 </style>
