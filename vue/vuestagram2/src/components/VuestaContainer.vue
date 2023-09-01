0fb80fa42199a58cdbdc7c3c20decfc2eb1e6374
York_Ki <p.ryan9311@gmail.com>
2023-09-01 21:24:29 +0900

    인스타 레이아웃 작업

---
+++ vue/vuestagram2/src/components/VuestaContainer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    {{ console.log(vuesta[0]) }}
+    <ContentsPost :vuesta="vuesta" />
+    <ContentsPost />
+    <ContentsPost />
+  </div>
+</template>
+
+<script>
+import ContentsPost from "./Post.vue";
+
+export default {
+  name: "VuestaContainer",
+  components: {
+    ContentsPost,
+  },
+  props: {
+    vuesta: Array,
+  },
+};
+</script>
+
+<style></style>
